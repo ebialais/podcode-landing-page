c56df0cec6da98b72d7968ff2dea1816cef33178
Andrés Goldestein <andresgoldestein@gmail.com>
2019-03-27 10:19:01 +0100

    marge texte

--- src/components/Emissions.vue
+++ src/components/Emissions.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="emissions">
-        <p>Une emission au top !</p>
+        <p>Derniers épisodes</p>
         <illu2 />
 
        <iframe width="80%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/590494680&color=%23b6bec8&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
@@ -34,12 +34,16 @@
     box-shadow: 0 0 11px rgba(33,33,33,.2); 
     transition: box-shadow .3s;
     z-index: -50;
+    border-radius: 10px;
   }
 
   #emissions:hover {
     box-shadow: 0 0 11px rgba(3, 53, 148, 0.2); 
   }
 
-
+  p {
+    font-size: 0.5em;
+    margin-top: 15px;
+  }
 
 </style>
