8b3e0a295861b07e88d7a39efcbcc2cd1cf1d3fa
Emilie Bialais <ebialais@gmail.com>
2019-05-30 11:25:29 +0200

    screenshot for readme

--- src/components/ChoixEpisodes.vue
+++ src/components/ChoixEpisodes.vue
@@ -16,7 +16,7 @@
             allow="autoplay" 
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/590494680&color=%23b6bec8&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
         </iframe>
-        <iframe 
+        <!-- <iframe 
             width="80%" 
             height="166" 
             scrolling="no" 
@@ -55,7 +55,7 @@
             frameborder="no" 
             allow="autoplay" 
             src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/590494680&color=%23b6bec8&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true">
-        </iframe>
+        </iframe> -->
     </div>
 </template>
 
